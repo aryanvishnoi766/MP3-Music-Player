c9cbd70f6fd33488fd2e021a608b741aced33b40
Aryan Vishnoi <77731029+aryanvishnoi766@users.noreply.github.com>
2025-01-07 18:39:28 +0530

    Update quary.sql

--- Database Code/quary.sql
+++ Database Code/quary.sql
@@ -1,1 +1,9 @@
-
+use login_schema;
+show tables;
+CREATE table users(
+idusers INT auto_increment,
+username varchar(255) NOT NULL,
+password varchar(255) Not Null,
+email varchar(255) Not Null,
+primary key(username));
+select* from users;
