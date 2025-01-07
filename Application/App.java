b3c0789f600001f4cb91280bf7e8730ab9c1db4f
Aryan Vishnoi <77731029+aryanvishnoi766@users.noreply.github.com>
2025-01-07 18:31:56 +0530

    Add files via upload

---
+++ Application/App.java
@@ -0,0 +1,16 @@
+package Application;
+import guis.LoginFormGUI;
+import javax.swing.*;
+public class App {
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable(){
+            @Override
+            public void run(){
+                new LoginFormGUI().setVisible(true);
+//                Song song = new Song("src/assets/Wind Riders - Asher Fulero.mp3");
+//                System.out.println(song.getSongTitle());
+//               System.out.println(song.getSongArtist());
+            }
+        });
+    }
+}
